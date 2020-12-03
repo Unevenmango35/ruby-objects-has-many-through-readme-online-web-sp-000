66714ea7eb45a8c6aa4e3153449a2ee1ffbeb3c8
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-03 08:32:08 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -19,5 +19,11 @@ class Meal
   def new_meal(waiter, total, tip=0)
    Meal.new(waiter, self, total, tip)
  end
+
+ def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
  
 end
