97125f91aed55236606c63c013f65c20c69cdf2d
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-03 08:23:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,15 +1,17 @@
 class Waiter
-    attr_accessor :name, :years
-
-    @@all = []
-
-    def initialize(name,years)
-      @name = name
-      @years = years
-      @@all << name
-    end
-
-    def self.all
-      @@all
-    end
+ 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
 end
